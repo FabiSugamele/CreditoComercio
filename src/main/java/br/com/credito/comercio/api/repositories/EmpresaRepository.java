7826b809a2c7479676976a2f964999e713a24509
Fabiana Sugamele <sugamele.fabiana@gmail.com>
2020-08-19 01:28:25 -0300

    Criado o repositorio da empresa

---
+++ src/main/java/br/com/credito/comercio/api/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package br.com.credito.comercio.api.repositories;
+
+import br.com.credito.comercio.api.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+
+    @Transactional(readOnly = true)
+    Empresa findByCnpj(String cnpj);
+}
